5ef9771015cba860811fb7341a874bb4e83b7876
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2024-11-22 21:58:26 +0530

    main game body started

--- src/components/MainGame.jsx
+++ src/components/MainGame.jsx
@@ -1,9 +1,11 @@
 import "./MainGame.css";
 import MainGame_head from "./MainGame_head";
+import MainGameBody from "./MainGameBody";
 
 export default function MainGame() {
   return <>
   {/* eslint-disable-next-line */}
     <MainGame_head/>
+    <MainGameBody/>
   </>;
 }
---
+++ src/components/MainGameBody.jsx
@@ -0,0 +1,13 @@
+import "./MainGameBody.css";
+
+export default function MainGameBody(){
+    return <main className="mainSingleDice">
+        <div className="singleDice">
+            <img src="./images/dice_1.png" alt="individual dice" />
+        </div>
+        <div>
+            click on DICE to roll
+        </div>
+
+    </main>
+}
