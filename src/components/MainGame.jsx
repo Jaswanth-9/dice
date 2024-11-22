9cb58812598eaca419b4e4a7ada30f390fcb3f84
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2024-11-22 20:05:45 +0530

    eslint removed error

--- src/components/MainGame.jsx
+++ src/components/MainGame.jsx
@@ -3,6 +3,7 @@ import MainGame_head from "./MainGame_head";
 
 export default function MainGame() {
   return <>
+  {/* eslint-disable-next-line */}
     <MainGame_head/>
   </>;
 }
--- src/components/MainGame_head.jsx
+++ src/components/MainGame_head.jsx
@@ -1,13 +1,20 @@
 import "./MainGame_head.css";
 
 export default function MainGame_head() {
+    const number = [1,2,3,4,5,6];
   return (
-    <main>
+    <main className="MainGame_head">
       <div className="score">
         <h1>0</h1>
         <h3>Total Score</h3>
       </div>
-      <div className="guessNumber"></div>
+      <div className="guessNumber">
+        {
+            number.map((value, i) => (
+                <div key = {i} > {value} </div>
+            ))
+        }
+      </div>
     </main>
   );
 }
