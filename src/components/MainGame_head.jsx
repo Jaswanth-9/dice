af06ce50edf5089285d4bf47dea8689f2cbfe89f
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2024-11-22 21:44:31 +0530

    main game head - finished

--- src/components/MainGame_head.jsx
+++ src/components/MainGame_head.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "./MainGame_head.css";
 
 export default function MainGame_head() {
-    const number = [1,2,3,4,5,6];
+  const number = [1, 2, 3, 4, 5, 6];
+  const [selectNumber, setselectNumber] = useState();
+
   return (
     <main className="MainGame_head">
       <div className="score">
@@ -9,11 +12,21 @@ export default function MainGame_head() {
         <h3>Total Score</h3>
       </div>
       <div className="guessNumber">
-        {
-            number.map((value, i) => (
-                <div key = {i} > {value} </div>
-            ))
-        }
+        <div className="numberBoxMain">
+          {number.map((value, i) => (
+            <div className="numberBox"
+              key={i}
+              onClick={() => setselectNumber(value)}
+              style={{
+                backgroundColor: value === selectNumber ? "black" : "white",
+                color: value === selectNumber ? "white" : "black",
+              }}
+            >
+              {value}
+            </div>
+          ))}
+        </div>
+        <p>select number</p>
       </div>
     </main>
   );
